67e7bfbf12be89a5c0b25b3a6a943772a32ff366
ShreyaaDangol <133582844+ShreyaaDangol@users.noreply.github.com>
2023-05-14 21:04:59 +0545

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForms() {
+    document.getElementById("myForm").style.display = "none";
+}
